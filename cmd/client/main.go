12592d51d3b6c3788ce7d08bacbbd13cf891ca84
diwakar kumar <developer.diwakar@gmail.com>
2024-10-28 06:49:31 +0530

    Search Laptop functionality implemented

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"flag"
+	"io"
 	"log"
 	"time"
 
@@ -14,21 +15,7 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-func main() {
-	serverAddress := flag.String("address", "", "the server address")
-	flag.Parse()
-	log.Printf("dial server %s", *serverAddress)
-
-	opts := []grpc.DialOption{}
-	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
-
-	conn, err := grpc.NewClient(*serverAddress, opts...)
-	if err != nil {
-		log.Fatal("cannot dial server: ", err)
-	}
-
-	laptopClient := pb.NewLaptopServiceClient(conn)
-
+func createLaptop(laptopClient pb.LaptopServiceClient) {
 	laptop := sample.NewLaptop()
 	req := &pb.CreateLaptopRequest{
 		Laptop: laptop,
@@ -48,4 +35,64 @@ func main() {
 		return
 	}
 	log.Printf("Laptop created with id: %s", res.Id)
+}
+
+func searchLaptop(laptopClient pb.LaptopServiceClient, filter *pb.Filter) {
+	log.Print("search filter: ", filter)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	req := &pb.SearchLaptopRequest{Filter: filter}
+	stream, err := laptopClient.SearchLaptop(ctx, req)
+	if err != nil {
+		log.Fatal("cannot receive response: ", err)
+	}
+
+	for {
+		res, err := stream.Recv()
+		if err == io.EOF {
+			return
+		}
+		if err != nil {
+			log.Fatal("cannot receive response: ", err)
+		}
+
+		laptop := res.GetLaptop()
+		log.Print(" - found: ", laptop.GetId())
+		log.Print(" + brand: ", laptop.GetBrand())
+		log.Print(" + name: ", laptop.GetName())
+		log.Print(" + cpu cores: ", laptop.GetCpu().GetNumberCores())
+		log.Print(" + cpu min ghz: ", laptop.GetCpu().GetMinGhz())
+		log.Print(" + ram: ", laptop.GetRam().GetValue(), laptop.GetRam().GetUnit())
+		log.Print(" + price: ", "$", laptop.GetPriceUsd())
+	}
+}
+
+func main() {
+	serverAddress := flag.String("address", "", "the server address")
+	flag.Parse()
+	log.Printf("dial server %s", *serverAddress)
+
+	opts := []grpc.DialOption{}
+	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	conn, err := grpc.NewClient(*serverAddress, opts...)
+	if err != nil {
+		log.Fatal("cannot dial server: ", err)
+	}
+
+	laptopClient := pb.NewLaptopServiceClient(conn)
+	// Create laptops
+	for i := 0; i < 10; i++ {
+		createLaptop(laptopClient)
+	}
+
+	// Search Laptops
+	filter := &pb.Filter{
+		MaxPriceUsd: 3000,
+		MinCpuCores: 4,
+		MinCpuGhz:   2.5,
+		MinRam:      &pb.Memory{Value: 8, Unit: pb.Memory_GIGABYTE},
+	}
+	searchLaptop(laptopClient, filter)
 }
--- service/laptop_store.go
+++ service/laptop_store.go
@@ -1,9 +1,12 @@
 package service
 
 import (
+	"context"
 	"errors"
 	"fmt"
+	"log"
 	"sync"
+	"time"
 
 	"github.com/developerdiwakar/pcbook/pb"
 	"github.com/jinzhu/copier"
@@ -17,6 +20,8 @@ type LaptopStore interface {
 	Save(laptop *pb.Laptop) error
 	// Find finds the laptop to the store
 	Find(id string) (*pb.Laptop, error)
+	// Search searches for laptops with filter, returns one by one via the found function
+	Search(ctx context.Context, filter *pb.Filter, found func(laptop *pb.Laptop) error) error
 }
 
 // InMemoryLaptopStore stores laptop in memory
@@ -42,8 +47,7 @@ func (store *InMemoryLaptopStore) Save(laptop *pb.Laptop) error {
 	}
 
 	// Deep copy
-	other := &pb.Laptop{}
-	err := copier.Copy(other, laptop)
+	other, err := deepCopy(laptop)
 	if err != nil {
 		return fmt.Errorf("cannot copy laptop data: %w", err)
 	}
@@ -52,6 +56,7 @@ func (store *InMemoryLaptopStore) Save(laptop *pb.Laptop) error {
 	return nil
 }
 
+// Find finds the laptop to the store
 func (store *InMemoryLaptopStore) Find(id string) (*pb.Laptop, error) {
 	store.mutex.RLock()
 	defer store.mutex.RUnlock()
@@ -63,11 +68,80 @@ func (store *InMemoryLaptopStore) Find(id string) (*pb.Laptop, error) {
 	}
 
 	// Deep copy
+	return deepCopy(laptop)
+}
+
+// Search searches for laptops with filter, returns one by one via the found function
+func (store *InMemoryLaptopStore) Search(ctx context.Context, filter *pb.Filter, found func(laptop *pb.Laptop) error) error {
+	store.mutex.RLock()
+	defer store.mutex.RUnlock()
+
+	for _, laptop := range store.data {
+		// simulate heavy processing
+		time.Sleep(time.Second)
+		log.Print("checking laptop id: ", laptop.GetId())
+		if ctx.Err() == context.Canceled || ctx.Err() == context.DeadlineExceeded {
+			log.Println("request is canceled or exceed deadline")
+			return errors.New("context is camceled")
+		}
+		if isQualified(filter, laptop) {
+			other, err := deepCopy(laptop)
+			if err != nil {
+				return err
+			}
+			err = found(other)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func isQualified(filter *pb.Filter, laptop *pb.Laptop) bool {
+	if laptop.GetPriceUsd() > filter.MaxPriceUsd {
+		return false
+	}
+	if laptop.GetCpu().NumberCores < filter.MinCpuCores {
+		return false
+	}
+	if laptop.GetCpu().GetMinGhz() < filter.MinCpuGhz {
+		return false
+	}
+
+	if toBit(laptop.GetRam()) < toBit(filter.GetMinRam()) {
+		return false
+	}
+
+	return true
+}
+
+func toBit(memory *pb.Memory) uint64 {
+	value := memory.GetValue()
+
+	switch memory.GetUnit() {
+	case pb.Memory_BIT:
+		return value // 1 bit
+	case pb.Memory_BYTE:
+		return value << 3 // 8 = 2^3 bits
+	case pb.Memory_KILOBYTE:
+		return value << 13 // 1024*8 = 2^10 + 2^3 = 2^13 bits
+	case pb.Memory_MEGABITE:
+		return value << 23 // 1024 * 1024 * 8 = 2^10 + 2^10 + 2^3 bits
+	case pb.Memory_GIGABYTE:
+		return value << 33 // 1024 * 1024 * 1024 * 8 bits
+	case pb.Memory_TERABYTE:
+		return value << 43 // 1024 * 1024 * 1024 * 1024 * 8 bits
+	default:
+		return 0
+	}
+}
+
+func deepCopy(laptop *pb.Laptop) (*pb.Laptop, error) {
 	other := &pb.Laptop{}
 	err := copier.Copy(other, laptop)
 	if err != nil {
 		return nil, fmt.Errorf("cannot copy laptop data: %w", err)
 	}
-
 	return other, nil
 }
